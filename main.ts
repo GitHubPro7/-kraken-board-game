702d36c024a04c99048715f1991b77612c84d8ee
GitHubPro7 <128514928+GitHubPro7@users.noreply.github.com>
2023-03-21 13:45:00 -0400

    added dice and spinner

--- main.ts
+++ main.ts
@@ -1,3 +1,130 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    if (randint(1, 8) == 1) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
+    }
+    if (randint(1, 8) == 2) {
+        basic.showLeds(`
+            . . # # #
+            . . . # #
+            . . # . #
+            . # . . .
+            # . . . .
+            `)
+    }
+    if (randint(1, 8) == 3) {
+        basic.showLeds(`
+            . . # . .
+            . . # # .
+            # # # # #
+            . . # # .
+            . . # . .
+            `)
+    }
+    if (randint(1, 8) == 4) {
+        basic.showLeds(`
+            # . . . .
+            . # . . .
+            . . # . #
+            . . . # #
+            . . # # #
+            `)
+    }
+    if (randint(1, 8) == 5) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (randint(1, 8) == 6) {
+        basic.showLeds(`
+            . . . . #
+            . . . # .
+            # . # . .
+            # # . . .
+            # # # . .
+            `)
+    }
+    if (randint(1, 8) == 7) {
+        basic.showLeds(`
+            . . # . .
+            . # # . .
+            # # # # #
+            . # # . .
+            . . # . .
+            `)
+    }
+    if (randint(1, 8) == 8) {
+        basic.showLeds(`
+            # # # . .
+            # # . . .
+            # . # . .
+            . . . # .
+            . . . . #
+            `)
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    if (randint(1, 6) == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (randint(1, 6) == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . . . .
+            . # . . .
+            . . . . .
+            `)
+    }
+    if (randint(1, 6) == 3) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . # . .
+            . # . . .
+            . . . . .
+            `)
+    }
+    if (randint(1, 6) == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    }
+    if (randint(1, 6) == 5) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+    }
+    if (randint(1, 6) == 6) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . # . # .
+            . # . # .
+            . . . . .
+            `)
+    }
 })
